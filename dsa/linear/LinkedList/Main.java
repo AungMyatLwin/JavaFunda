29ccc4c1ad29fc32dec3a6de8ee77c0b0ac1cccb
AungMyatLwin <zack.cloud7@gmail.com>
2023-01-26 10:17:35 +0630

    added toArray method

--- dsa/linear/LinkedList/Main.java
+++ dsa/linear/LinkedList/Main.java
@@ -1,5 +1,7 @@
 package dsa.linear.LinkedList;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String [] args){
       var test= new LinkedListEx();
@@ -7,12 +9,15 @@ public class Main {
       test.addLast(10);
       test.addLast(20);
       test.addLast(30);
+      var array=test.toArray();
       System.out.println(test.size());
+      System.out.println(Arrays.toString(array));
       test.removeFirst();
       test.removeLast();
       System.out.println(test.indexOf(40));
       System.out.println(test.contains(40));
       System.out.println(test.size());
-
+      
+      System.out.println(Arrays.toString(array));
     }
 }
